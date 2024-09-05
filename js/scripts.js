6812a772d13524d1015d186457b3568c7ca295d1
javi <javi3gp@gmail.com>
2024-09-05 20:57:55 +0200

    Pruebas con dark-light mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
 // Estos son los scripts de mi archivo de JavaScript
+console.log("hola"); 
+// Selecciona el botón
+const themeToggleButton = document.getElementById('themeImage');
+
+// Agrega un evento de clic al botón
+themeToggleButton.addEventListener('click', function () {
+    // Alterna la clase "dark-mode" en el body
+    document.body.classList.toggle('dark-mode');
+    
+    // Cambia el texto del botón
+    if (document.body.classList.contains('dark-mode')) {
+        themeImage.src = 'ico/moon.png';
+    } else {
+        themeImage.src = 'ico/sun.png';
+    }
+});
