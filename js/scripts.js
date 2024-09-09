e7df4caa5f1ea53c39ccfa18e52459ad56592dbc
javi <javi3gp@gmail.com>
2024-09-09 20:45:05 +0200

    Prueba enlace a Figma, prueba tipografía SUSE, anclas con delay.

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,10 @@ const themeToggleButton = document.getElementById('themeImage');
 // Agrega un evento de clic al botón
 themeToggleButton.addEventListener('click', function () {
     // Alterna la clase "dark-mode" en el body
-    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
     
     // Cambia el texto del botón
-    if (document.body.classList.contains('dark-mode')) {
+    if (document.body.classList.contains('light-mode')) {
         themeImage.src = 'ico/moon.png';
     } else {
         themeImage.src = 'ico/sun.png';
